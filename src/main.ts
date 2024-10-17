9543c998483ccaf4d90bbbdcab22cce1e7c1d5cc
Stilllee <orodae@gmail.com>
2024-10-17 18:50:22 +0900

    [chore] Swagger 설치 및 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,40 @@
-import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // CORS 활성화
+  app.enableCors();
+
+  // 모든 요청의 content-type을 application/json으로 설정
+  app.use((req, res, next) => {
+    req.headers['content-type'] = 'application/json';
+    next();
+  });
+
+  // Validation Pipe 설정
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('TASTORAGE API')
+    .setDescription('테이스토리지 API 문서입니다.')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(12345);
 }
 bootstrap();
