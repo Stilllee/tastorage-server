3fe3c498a3d7bcf3322cf89fbf4487d932c7dfc8
Stilllee <orodae@gmail.com>
2024-12-11 09:55:49 +0900

    [chore] vercel 배포 설정
    - port 동적 할당을 위한 main.ts 수정
    - prisma generate 명령어 추가
    - vercel.json 설정

--- src/main.ts
+++ src/main.ts
@@ -12,12 +12,6 @@ async function bootstrap() {
   // CORS 활성화
   app.enableCors();
 
-  // 모든 요청의 content-type을 application/json으로 설정
-  app.use((req, res, next) => {
-    req.headers['content-type'] = 'application/json';
-    next();
-  });
-
   // Validation Pipe 설정
   app.useGlobalPipes(
     new ValidationPipe({
@@ -41,6 +35,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(12345);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
