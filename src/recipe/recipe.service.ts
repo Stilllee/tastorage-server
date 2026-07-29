070a93bca3e10f9aec6449c3d40a4653397f551c
Stilllee <orodae@gmail.com>
2026-07-29 16:24:30 +0900

    fix: TypeScript baseUrl 사용 제거

--- src/recipe/recipe.service.ts
+++ src/recipe/recipe.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { CreateRecipeDto } from './dto/create-recipe.dto';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { UpdateRecipeDto } from './dto/update-recipe.dto';
 
 @Injectable()
